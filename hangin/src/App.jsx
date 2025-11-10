fbdcd274bb1fcef2bbf0e91cab331eb3c4510cf4
Gabriel John Solomon <153481400+Gabi-comm@users.noreply.github.com>
2025-11-10 16:42:12 +0800

    Implemented the Login and sign in
    Connected to the supabase

---
+++ hangin/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "./components/login";       
+import "./components/loginstyle.css";         
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ hangin/src/components/loginindex.jsx
@@ -0,0 +1,12 @@
+// loginindex.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Login from './login';
+import './loginstyle.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>
+);
